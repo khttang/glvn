dc576fee862ae30f0f3f200106e8945238c98be3
Tang, Khiem <Khiem_Tang@intuit.com>
2016-04-28 00:42:38 -0700

    Added back in Cody's change.

--- config/env/development.js
+++ config/env/development.js
@@ -15,6 +15,7 @@ module.exports = {
 db: {
     //uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
     //uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || '104.236.175.61') + '/glvn',
+    //uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || '104.236.175.61') + '/mean-dev',
     uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/glvn',
 
   options: {
