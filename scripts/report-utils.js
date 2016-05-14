3fd67abc3bc6720ccce99551029cbf2fe85d2ef3
Tang, Khiem <Khiem_Tang@intuit.com>
2016-05-13 23:02:37 -0700

    Added a script for summary report

---
+++ scripts/report-utils.js
@@ -0,0 +1,104 @@
+var mongoose = require('mongoose'),
+    chalk = require('chalk'),
+    mg = require('../config/lib/mongoose'),
+    fs = require('fs'),
+    HashMap = require('hashmap');
+
+mg.loadModels();
+
+mg.connect(function (db) {
+
+    var User = mongoose.model('User');
+    var Student = mongoose.model('Student');
+    var Registration = mongoose.model('Registration');
+
+    var beginDate = new Date('2016-05-11');
+    var endDate = new Date('2016-05-15');
+
+    Registration.find({
+        /*
+        'received' : {
+            $lt: endDate,
+            $gte: beginDate
+        }
+        */
+        'status': 'APPROVED'
+    }, function(err, docs) {
+        if (!err) {
+
+            var feeCollected = 0;
+            var exempts = new Number(0);
+            var mapClasses = new HashMap();
+
+            for (var i = 0, len = docs.length; i < len; i++) {
+                var reg = docs[i];
+                feeCollected += new Number(reg.regFee);
+                if (reg.regTeacherExempt) {
+                    exempts += 1;
+                }
+
+                var counter;
+                if (reg.glClass !== null) {
+                    counter = mapClasses.get(reg.glClass);
+                    if (counter === undefined) {
+                        counter = new Number(0);
+                    }
+                    counter += 1;
+                    mapClasses.set(reg.glClass, counter);
+                }
+
+                if (reg.vnClass !== null) {
+                    counter = mapClasses.get(reg.vnClass);
+                    if (counter === undefined) {
+                        counter = new Number(0);
+                    }
+                    counter += 1;
+                    mapClasses.set(reg.vnClass, counter);
+                }
+            }
+
+            console.log('Summary Report');
+            mapClasses.forEach(function(value, key) {
+                console.log(key + " : " + value);
+            });
+
+            console.log('Registrations received: '+ docs.length);
+            console.log('Fees collected: ' + feeCollected + '$');
+            console.log('Teacher exempts: ' + exempts);
+        }
+    });
+
+
+
+/*
+    for (var i = 0, len = importJson.length; i < len; i++) {
+        var data = importJson[i];
+        var user = new User(data.user);
+        user.save(function (err) {
+            if (err) {
+                throw new Error("Can't save user: "+ err);
+            }
+        });
+    }
+
+    for (var i = 0, len = importJson.length; i < len; i++) {
+        var data = importJson[i];
+        var student = new Student(data.student);
+        student.save(function (err) {
+            if (err) {
+                throw new Error("Can't save student: " + err);
+            }
+        });
+    }
+
+    for (var i = 0, len = importJson.length; i < len; i++) {
+        var data = importJson[i];
+        var registration = new Registration(data.registration);
+        registration.save(function (err) {
+            if (err) {
+                throw new Error("Can't save registration: " + err);
+            }
+        });
+    }
+    */
+});
