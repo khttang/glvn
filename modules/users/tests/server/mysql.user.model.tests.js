1d354c190bf4eaf4072ee6d46ef2a56f7cbd48aa
Tang, Khiem <Khiem_Tang@intuit.com>
2017-07-17 17:18:50 -0700

    Added unit tests file to start working on mysql.

---
+++ modules/users/tests/server/mysql.user.model.tests.js
@@ -0,0 +1,17 @@
+
+
+describe('Admin Capabilities Unit Tests:', function () {
+    before(function () {
+
+    });
+
+    describe('Method Save', function () {
+        it('should begin with no users', function (done) {
+            done();
+        });
+    });
+
+    after(function (done) {
+        done();
+    });
+});
